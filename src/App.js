d07ba10f9e8cda0ba5e5f3d3a878237e47b6dc6a
Eli Weber <eliweber2001@gmail.com>
2022-06-05 21:09:49 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+function App()
+{
+  let [list, setList] = React.useState(()=>
+  {
+    let string = localStorage.getItem("ToDoList")
+    let data = JSON.parse(string)
+    return data
+  })
+  let [currentItem, setCurrentItem] = React.useState(()=>
+  {
+    return list ? list[0] : []
+  })
+
+  let [date, setDate] = React.useState(()=>
+  {
+    let date = new Date().toLocaleTimeString()
+    return date
+  })
+
+  
+  setInterval(()=>
+  {
+    setDate(()=>
+    {
+      return new Date().toLocaleTimeString()
+    })
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ReactJS</h1>
+      <h1>{date}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
